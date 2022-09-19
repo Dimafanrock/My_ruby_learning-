a5de376dde5dcdb09f1f6fffbc76836e56fe7726
Dimafanrock <dimafanrock1@gmail.com>
2022-09-19 14:21:18 +0300

    string 4

---
+++ strings_4.rb
@@ -0,0 +1,14 @@
+@string_array  =  ["String one", "String two","String tree" ]
+
+def insert_in_array(array  = @string_array,my_string)
+    array.push(my_string)
+end 
+
+def delete_from_array(array = @string_array,my_string)
+    array.delete_if {|i| i == my_string}
+end
+
+insert_in_array( @string_array ,"Test one")
+puts @string_array
+delete_from_array( @string_array ,"Test one")
+puts @string_array
