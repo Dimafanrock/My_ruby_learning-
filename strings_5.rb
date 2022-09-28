7c586f17de09c8fd835e53d7fc475d1668567c04
Dmytro Voznyy <dimafanrock1@gmail.com>
2022-09-28 13:29:25 +0300

    remove

--- strings_5.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-def testing_string_by_temples(my_string)
-  puts my_string.count(')').even? ? 'Correct count ' : ' Incorrect count'
-end
-
-puts 'Enter your text:'
-my_string = gets.chomp
-testing_string_by_temples(my_string)
